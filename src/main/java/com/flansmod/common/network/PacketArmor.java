37e00849164a1f156fa3627c1719c1bf8497b213
Charpy <76815325+Charpyy@users.noreply.github.com>
2024-02-11 20:17:27 +0100

    Update PacketArmor.java

--- src/main/java/com/flansmod/common/network/PacketArmor.java
+++ src/main/java/com/flansmod/common/network/PacketArmor.java
@@ -7,12 +7,26 @@ import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.item.ItemStack;
 import net.minecraft.network.play.server.SPacketEntityEquipment;
 
+import net.minecraft.entity.Entity;
+import net.minecraft.client.entity.EntityPlayerSP;
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.item.ItemStack;
+import net.minecraft.network.play.server.SPacketEntityEquipment;
+import net.minecraft.inventory.EntityEquipmentSlot;
+
 public class PacketArmor {
+
 	public static void disableEquipmentPackets(Entity player) {
-		((EntityPlayerMP) player).connection.sendPacket(new SPacketEntityEquipment(player.getEntityId(), EntityEquipmentSlot.MAINHAND, ItemStack.EMPTY));
-		((EntityPlayerMP) player).connection.sendPacket(new SPacketEntityEquipment(player.getEntityId(), EntityEquipmentSlot.HEAD, ItemStack.EMPTY));
-		((EntityPlayerMP) player).connection.sendPacket(new SPacketEntityEquipment(player.getEntityId(), EntityEquipmentSlot.CHEST, ItemStack.EMPTY));
-		((EntityPlayerMP) player).connection.sendPacket(new SPacketEntityEquipment(player.getEntityId(), EntityEquipmentSlot.LEGS, ItemStack.EMPTY));
-		((EntityPlayerMP) player).connection.sendPacket(new SPacketEntityEquipment(player.getEntityId(), EntityEquipmentSlot.FEET, ItemStack.EMPTY));
+		if (player instanceof EntityPlayerMP) {
+			EntityPlayerMP playerMP = (EntityPlayerMP) player;
+			playerMP.connection.sendPacket(new SPacketEntityEquipment(player.getEntityId(), EntityEquipmentSlot.MAINHAND, ItemStack.EMPTY));
+			playerMP.connection.sendPacket(new SPacketEntityEquipment(player.getEntityId(), EntityEquipmentSlot.HEAD, ItemStack.EMPTY));
+			playerMP.connection.sendPacket(new SPacketEntityEquipment(player.getEntityId(), EntityEquipmentSlot.CHEST, ItemStack.EMPTY));
+			playerMP.connection.sendPacket(new SPacketEntityEquipment(player.getEntityId(), EntityEquipmentSlot.LEGS, ItemStack.EMPTY));
+			playerMP.connection.sendPacket(new SPacketEntityEquipment(player.getEntityId(), EntityEquipmentSlot.FEET, ItemStack.EMPTY));
+		} else if (player instanceof EntityPlayerSP) {
+			EntityPlayerSP playerSP = (EntityPlayerSP) player;
+			playerSP.inventory.offHandInventory.set(0, ItemStack.EMPTY);
+		}
 	}
 }
