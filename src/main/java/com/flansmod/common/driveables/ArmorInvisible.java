3a0c05f7df8567f0df14475dd6332b62d4d6858f
Charpy <76815325+Charpyy@users.noreply.github.com>
2024-02-14 20:38:34 +0100

    Update ArmorInvisible.java

--- src/main/java/com/flansmod/common/driveables/ArmorInvisible.java
+++ src/main/java/com/flansmod/common/driveables/ArmorInvisible.java
@@ -15,21 +15,29 @@ import java.util.List;
 		public static List<ItemStack> retrieveArmorItems(EntityPlayer player) {
 			List<ItemStack> armorItems = new ArrayList<>();
 			NonNullList<ItemStack> armorInventory = player.inventory.armorInventory;
-
-			for (int i = 0; i < armorInventory.size(); i++) {
-				//armorItems.add(armorInventory.get(i));
-				armorItems.add(player.inventory.armorInventory.get(i));
-				armorInventory.set(i, ItemStack.EMPTY);
-				player.sendMessage(new TextComponentString(armorItems.toString()));
-			}
-
+			armorItems.add(player.inventory.armorInventory.get(0));
+			armorItems.add(player.inventory.armorInventory.get(1));
+			armorItems.add(player.inventory.armorInventory.get(2));
+			armorItems.add(player.inventory.armorInventory.get(3));
+			armorInventory.set(0, ItemStack.EMPTY);
+			armorInventory.set(1, ItemStack.EMPTY);
+			armorInventory.set(2, ItemStack.EMPTY);
+			armorInventory.set(3, ItemStack.EMPTY);
+			player.sendMessage(new TextComponentString(armorItems.toString()));
 			return armorItems;
 		}
 
 		public static void restoreArmorItems(EntityPlayer player, List<ItemStack> armorItems) {
+			player.sendMessage(new TextComponentString(armorItems.toString()));
+			if (player == null || player.inventory == null || player.inventory.armorInventory == null) {
+				return;
+			}
+			if (armorItems == null) {
+				return;
+			}
 			NonNullList<ItemStack> armorInventory = player.inventory.armorInventory;
-			if (armorItems != null) {
-				for (int i = 0; i < armorInventory.size() && i < armorItems.size(); i++) {
+			for (int i = 0; i < 4; i++) {
+				if (i < armorItems.size() && armorItems.get(i) != null) {
 					armorInventory.set(i, armorItems.get(i));
 				}
 			}
