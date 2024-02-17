13442f17f2f8040201a011eb36c22f405e4bc19e
Charpy <76815325+Charpyy@users.noreply.github.com>
2024-02-17 13:38:23 +0100

    Update ArmorInvisible.java

--- src/main/java/com/flansmod/common/driveables/ArmorInvisible.java
+++ src/main/java/com/flansmod/common/driveables/ArmorInvisible.java
@@ -3,6 +3,7 @@ package com.flansmod.common.driveables;
 import com.google.common.cache.CacheBuilder;
 import com.google.common.cache.LoadingCache;
 import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.client.renderer.entity.RenderLivingBase;
 import net.minecraft.client.renderer.entity.RenderPlayer;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
@@ -14,32 +15,33 @@ import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+import java.util.HashSet;
+import java.util.Set;
 
 
 public class ArmorInvisible {
-	@Mod.EventBusSubscriber
 	public static class EventHandler {
 
-		private static boolean renderArmor = true;
+		private static final Set<EntityLivingBase> invisibleEntities = new HashSet<>();
 
+		@SideOnly(Side.CLIENT)
 		@SubscribeEvent
-		public static void onRenderPlayer(RenderPlayerEvent.Pre event) {
-			if (!renderArmor) {
-				return;
-			}
-
-			EntityPlayer player = event.getEntityPlayer();
-			RenderPlayer renderer = event.getRenderer();
-			if (player != null && renderer != null) {
-				for (ItemStack stack : player.getArmorInventoryList()) {
-					if (!stack.isEmpty()) {
-						stack.setCount(0);
-					}
-				}
+		public static void onRenderPlayer(RenderLivingEvent.Pre<EntityLivingBase> event) {
+			EntityLivingBase entity = event.getEntity();
+			if (entity != null && invisibleEntities.contains(entity)) {
+				event.setCanceled(true);
 			}
 		}
-		public static void setArmor(EntityPlayer player, boolean invisible) {
-			renderArmor = invisible;
+
+		public static void setArmor(EntityLivingBase entity, boolean invisible) {
+			if (invisible) {
+				invisibleEntities.add(entity);
+			} else {
+				invisibleEntities.remove(entity);
+			}
 		}
 	}
 }
